ff6a1768e3346b54a03a9d18581095d2d7341038
MartaZare <marta.zarec@gmail.com>
2023-10-19 09:19:44 +0300

    Implemented chart error handling logic

--- src/pages/history/Chart.tsx
+++ src/pages/history/Chart.tsx
@@ -16,25 +16,35 @@ interface ChartProps {
 function Chart(props: ChartProps) {
   return (
     <div className="chart">
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
-          width={800}
-          height={400}
-          data={props.historyArray}
-          margin={{ top: 50, left: 0, right: 0, bottom: 40 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <YAxis dataKey="time" tick={{ fill: "#ffffff" }} />
-          <XAxis dataKey="date" tick={{ display: "none" }} />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="time"
-            stroke="#ffffff"
-            fill="#e25788"
-          />
-        </AreaChart>
-      </ResponsiveContainer>
+      {props.historyArray.length === 0 ? (
+        <>
+          <h2>No Results to Display</h2>
+          <p>
+            If you already completed at least one study session try reloading
+            the page.
+          </p>
+        </>
+      ) : (
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={800}
+            height={400}
+            data={props.historyArray}
+            margin={{ top: 20, left: -10, right: 10, bottom: -10 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <YAxis dataKey="time" tick={{ fill: "#ffffff" }} />
+            <XAxis dataKey="date" tick={{ display: "none" }} />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="time"
+              stroke="#ffffff"
+              fill="#e25788"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      )}
     </div>
   );
 }
